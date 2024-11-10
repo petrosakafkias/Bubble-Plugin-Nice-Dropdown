db0734f2ca4ed0003d81586d9a041710901bb828
petrosakafkias <153187839+petrosakafkias@users.noreply.github.com>
2024-11-10 20:13:26 +0200

    Update preview.js

--- elements/AAD-850m8/preview.js
+++ elements/AAD-850m8/preview.js
@@ -1,16 +1,19 @@
 function(instance, properties) {
     
-    console.log('preview settings')
+    console.log('preview settings');
     
-    let div = document.createElement('div')
-    div.innerText = 'DROPDOWN'
+    let div = document.createElement('div');
+    div.innerText = 'DROPDOWN';
 
-    // div style 
-    div.style.fontFamily = properties.bubble.font_face().split(':::')[0]
-    div.style.fontSize = properties.bubble.font_size()+'px'
-    div.style.color = properties.bubble.font_color()
-    instance.canvas.append(div)
-    console.log(properties.bubble.font_face().split(':::')[0])
-
-
+    // Set div style
+    div.style.fontFamily = properties.bubble.font_face().split(':::')[0];
+    div.style.fontSize = properties.bubble.font_size() + 'px';
+    div.style.color = properties.bubble.font_color();
+    
+    // Ensure div appears above all other elements
+    div.style.position = 'absolute';
+    div.style.zIndex = '9999'; // High z-index to bring it above other elements
+    
+    instance.canvas.append(div);
+    console.log(properties.bubble.font_face().split(':::')[0]);
 }
